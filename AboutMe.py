688ef1281597eaa5e9890afd957fb9cfcadec629
imran_mediusware <imran.mediusware@gmail.com>
2018-08-11 00:00:40 +0600

    about me

---
+++ AboutMe.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+print('Hello!')
+print('''
+That's my dog
+
+O____
+ ||||`
+
+I Live In Dhaka and I like songs
+
+╔══╗ 
+║██║ 
+║(O)║♫ ♪ ♫ ♪
+╚══╝
+''')
+
+birth_year = input("I can tell your age. Say me your birth year :)\n")
+age = datetime.now().year - int(birth_year)
+print("Your age is", age)
+
+print("If you were a dog, you'd be", age * 7,"!!")
+print("Ha " * 3)
